69a30c297600bc8f41d118ef71047846ccc8b5e7
Kesavan-app <yogachendiran777@gmail.com>
2025-08-11 18:35:06 +0530

    file added

---
+++ script.js
@@ -0,0 +1,56 @@
+const text = document.querySelector(".text");
+const words = ["web Developer", "UI/UX Designer", "Flutter Developer"];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeEffect() {
+  let currentWord = words[wordIndex];
+  let displayText = currentWord.substring(0, charIndex);
+
+  text.innerHTML = displayText;
+
+  if (!isDeleting && charIndex < currentWord.length) {
+    charIndex++;
+    setTimeout(typeEffect, 100);
+  } else if (isDeleting && charIndex > 0) {
+    charIndex--;
+    setTimeout(typeEffect, 50);
+  } else {
+    isDeleting = !isDeleting;
+    if (!isDeleting) wordIndex = (wordIndex + 1) % words.length;
+    setTimeout(typeEffect, 800);
+  }
+}
+
+typeEffect();
+
+// New function for footer icon click
+function openLink(url) {
+  window.open(url, "_blank"); // Opens in new tab
+}
+
+function handleDownload() {
+  const confirmDownload = confirm("Do you want to download Kesavan's resume?");
+  if (confirmDownload) {
+    const link = document.createElement("a");
+    link.href = "assets/kesavan_m_res_final.pdf"; // File path in your project
+    link.download = "kesavan_Resume.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+}
+
+// Attach click event in normal JS
+document.getElementById("downloadBtn").addEventListener("click", handleDownload);
+
+/* ===== ✅ Hamburger Menu Script ===== */
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navbar = document.querySelector('.navbar');
+
+  menuToggle.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+  });
+});
